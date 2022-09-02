cebfb171d4b4c79ed862389fd9c384f9476d9781
pur100 <hgmancini@gmail.com>
2022-09-02 09:50:06 +0200

    run unit and system tests on GHA

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "products#index" 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :products, only: [:new, :create, :show]
   # Defines the root path route ("/")
   # root "articles#index"
 end
